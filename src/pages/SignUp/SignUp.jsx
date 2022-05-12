619b72b6570c628460db57f311c4fdcdadd857e5
TheAmlanSaikia <83667291+TheAmlanSaikia@users.noreply.github.com>
2022-05-12 11:54:16 +0530

    feat: SignUp form functionality

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,22 +1,79 @@
-import { useState } from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 import "./SignUp.css";
 
 const SignUp = () => {
+  const SIGN_UP = process.env.REACT_APP_SKORHEAD_SIGNUP;
+  const initialForm = {
+    username: "",
+    email: "",
+    password: "",
+    confirmpassword: "",
+  };
+  const [formdata, setFormData] = useState(initialForm);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
 
+  const handleChange = (event) => {
+    const { id, value } = event.target;
+    setFormData({ ...formdata, [id]: value });
+  };
 
- const [ formdata, setFormData ] = useState({
-     username:"",
-     email:"",
-     password:"",
-     confirmpassword:""
- })
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formdata));
+    setIsSubmit(true);
+  };
+
+  useEffect(() => {
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      signupSuccess();
+    }
+  }, [formErrors]);
+
+  const validate = (values) => {
+    const regexvalue =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const errors = {};
+    if (!values.username) {
+      errors.username = "Username is required";
+    }
+    if (!values.email) {
+      errors.email = "Email is required";
+    } else if (!regexvalue.test(values.email.trim())) {
+      errors.email = "This is not a valid email format!";
+    }
+    if (!values.password) {
+      errors.password = "Password is required";
+    } else if (values.password.length < 6) {
+      errors.password = "Passwords must be more than 6 characters";
+    }
+    if (!values.confirmpassword) {
+      errors.confirmpassword = "Password is required";
+    } else if (values.confirmpassword !== values.password) {
+      errors.confirmpassword = "Password doesnt match";
+    }
+    return errors;
+  };
+
+  const signupSuccess = async () => {
+    try {
+      const {
+        data: { encodedToken,createdUser },
+      } = await axios.post(SIGN_UP, formdata);
+      localStorage.setItem("token", encodedToken);
+      console.log(encodedToken, createdUser)
+    } catch (error) {
+        console.log(error);
+      }
+  };
 
   return (
     <div className='grid-login-container'>
       <main className='main main-login-container'>
         <div className='display-container-form'>
           <main className='form-container signup-form'>
-            <form id='form' className='form'>
+            <form id='form' className='form' onSubmit={handleSubmit}>
               <h3 className='title-text'> Register Here </h3>
               <section className='form-control form-control-username'>
                 <label for='username' className='form-label username-label'>
@@ -28,9 +85,9 @@ const SignUp = () => {
                   id='username'
                   value={formdata.username}
                   placeholder='Enter Username'
-                  onChange={(e)=> setFormData({...formdata,username: e.target.value})}
+                  onChange={handleChange}
                 />
-                <small className='error-msg'> Error Message </small>
+                <small className='class-error'>{formErrors.username}</small>
               </section>
               <section className='form-control form-control-email'>
                 <label for='email' className='form-label email-label'>
@@ -42,9 +99,9 @@ const SignUp = () => {
                   id='email'
                   value={formdata.email}
                   placeholder='Enter email'
-                  onChange={(e)=> setFormData({...formdata,email: e.target.value})}
+                  onChange={handleChange}
                 />
-                <small className='error-msg'> Error Message </small>
+                <small className='class-error'>{formErrors.email}</small>
               </section>
               <section className='form-control form-control-password1'>
                 <label for='password1' className='form-label pasword1-label'>
@@ -53,10 +110,12 @@ const SignUp = () => {
                 <input
                   type='password'
                   className='form-input password1-input'
-                  id='password1'
+                  id='password'
+                  value={formdata.password}
                   placeholder='Enter Password'
+                  onChange={handleChange}
                 />
-                <small className='error-msg'> Error Message </small>
+                <small className='class-error'>{formErrors.password}</small>
               </section>
               <section className='form-control form-control-password2'>
                 <label for='password2' className='form-label password2-label'>
@@ -65,10 +124,14 @@ const SignUp = () => {
                 <input
                   type='password'
                   className='form-input password2-input'
-                  id='password2'
+                  id='confirmpassword'
+                  value={formdata.confirmpassword}
                   placeholder='Confirm Password'
+                  onChange={handleChange}
                 />
-                <small className='error-msg'> Error Message </small>
+                <small className='class-error'>
+                  {formErrors.confirmpassword}
+                </small>
               </section>
               <button className='btn-submit' type='submit'>
                 Submit
@@ -76,7 +139,6 @@ const SignUp = () => {
             </form>
           </main>
         </div>
-        {console.log(formdata)}
       </main>
     </div>
   );
