48aec2372403f6c224260600866ba23487ed84a8
TheAmlanSaikia <83667291+TheAmlanSaikia@users.noreply.github.com>
2022-05-30 12:17:31 +0530

    fix: minor bugs and refactor

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -46,7 +46,7 @@ const Navigation = () => {
           ) : (
             <div className='link navbar-link dropdown'>
               <i className='fa-solid fal-lg fa-user'></i>
-              <p className='text-sm'>{user.firstName}</p>
+              <p className='text-sm'>{user}</p>
               <div className='dropdown-content'>
                 <p className='dropdown-tab pointer'>Products</p>
                 <p className='dropdown-tab pointer'>User</p>
